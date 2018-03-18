f1dbd8e9e3eb3b37f2daf0fe14ae946b4b58d8a0
David Polverari <david.polverari@gmail.com>
2018-03-18 15:25:16 -0300

    Add setup.py file to install fa-proxy
    Implement a simple setup.py file to allow for a 'pythonic' install
    procedure.
    
    Closes #2

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from distutils.core import setup
+
+setup(
+    name='fa-proxy',
+    version='0.1.0',
+    author='David da Silva Polverari',
+    author_email='david.polverari@gmail.com',
+    scripts=['fa-proxy'],
+    license='GNU General Public License, version 3'
+)
